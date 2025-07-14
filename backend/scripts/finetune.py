3c769d5051ffb68fcbb8c15f8f1d318d8da16a75
nevrohelios <dasharka05@gmail.com>
2025-07-15 01:27:20 +0530

    finetune working

--- backend/scripts/finetune.py
+++ backend/scripts/finetune.py
@@ -7,13 +7,25 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from transformers.trainer import Trainer
 from transformers.training_args import TrainingArguments
 
-data_dir = os.path.join(Path(os.getcwd()).parent, "data")
-
+parent_dir = Path(os.getcwd()).parent
+data_dir = os.path.join(parent_dir, "data")
+model_dir = os.path.join(parent_dir, "models")
+log_dir = os.path.join(parent_dir, "models", "logs")
+output_dir = os.path.join(parent_dir, "models", "finetuned")
 
 class CustomDataset(torch.utils.data.Dataset):
-    def __init__(self, data) -> None:
+    def __init__(self, data_path: str, tokenizer) -> None:
         super().__init__()
-        self.data = data
+        self.data = []
+        self.tokenizer = tokenizer
+        with open(data_path, 'r') as file:
+            for line in file:
+                item = json.loads(line)
+                self.data.append({
+                    'input_ids': torch.tensor(self.tokenizer(item['text'], truncation=True, padding="max_length", max_length=128)['input_ids']),
+                    'attention_mask': torch.tensor(self.tokenizer(item['text'], truncation=True, padding="max_length", max_length=128)['attention_mask']),
+                    'label': torch.tensor(int(item['label']))
+                })
 
     def __len__(self):
         return len(self.data)
@@ -27,37 +39,24 @@ def main():
     parser.add_argument("--data", type=str, default="data.jsonl", help="Data file name. Put the data file inside the `data` directory or `the absolute path`.")
     args = parser.parse_args()
     # datafile
-    data_file = os.path.join(data_dir, args.data)
-    if not os.path.exists(data_file):
+    data_path = os.path.join(data_dir, args.data)
+    if not os.path.exists(data_path):
         if os.path.exists(args.data): # check if the absolute path is provided
-            data_file = args.data
+            data_path = args.data
         else:
-            raise FileNotFoundError(f"Data file {data_file} does not exist. Please provide a valid path.")
-    
-    
-    with open(data_file, 'r') as file: # read the data
-        data = [json.loads(line) for line in file]
+            raise FileNotFoundError(f"Data file {data_path} does not exist. Please provide a valid path.")
 
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-    model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=5)
+    tokenizer = AutoTokenizer.from_pretrained(model_dir)
+    model = AutoModelForSequenceClassification.from_pretrained(model_dir, num_labels=2)  
 
-    # tokenize the data
-    tokenized_data = []
-    for item in data:
-        tmp = {
-            key: torch.tensor(val) for key, val in tokenizer(item['text'], truncation=True, padding="max_length", max_length=128).items()
-        }
-        tmp['label'] = torch.tensor(int(item['label']))
-        tokenized_data.append(tmp)
-    
-    dataset = CustomDataset(tokenized_data)
+    dataset = CustomDataset(data_path, tokenizer)
     training_args = TrainingArguments(
-        output_dir="./results",
+        output_dir=output_dir,
         num_train_epochs=3,
         per_device_train_batch_size=8,
         save_steps=10_000,
         save_total_limit=2,
-        logging_dir='./logs',
+        logging_dir=log_dir,
     )
 
     trainer = Trainer(
